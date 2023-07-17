82fe31022568ba0422f0e0d01225313a1614197a
Lucas Ruiz <deth_lucas@hotmail.com>
2023-07-17 18:54:36 -0300

    Report e summary

--- src/main/java/com/devsuperior/dsmeta/controllers/SaleController.java
+++ src/main/java/com/devsuperior/dsmeta/controllers/SaleController.java
@@ -1,14 +1,18 @@
 package com.devsuperior.dsmeta.controllers;
 
+import com.devsuperior.dsmeta.dto.SaleReportDTO;
+import com.devsuperior.dsmeta.dto.SaleSummaryDTO;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.devsuperior.dsmeta.dto.SaleMinDTO;
 import com.devsuperior.dsmeta.services.SaleService;
+
+import java.time.LocalDate;
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/sales")
@@ -22,16 +26,23 @@ public class SaleController {
 		SaleMinDTO dto = service.findById(id);
 		return ResponseEntity.ok(dto);
 	}
-
 	@GetMapping(value = "/report")
-	public ResponseEntity<?> getReport() {
-		// TODO
-		return null;
+	public ResponseEntity<Page<SaleReportDTO>>  getReport(
+			@RequestParam(name = "firstDate", defaultValue = "")String firstDate,
+			@RequestParam(name = "lastDate", defaultValue = "") String lastDate,
+			@RequestParam(name = "name" , defaultValue ="") String name,
+			Pageable pageable) {
+		Page<SaleReportDTO> dto = service.findBetweenDate(firstDate,lastDate,name,pageable);
+		return ResponseEntity.ok(dto);
 	}
 
 	@GetMapping(value = "/summary")
-	public ResponseEntity<?> getSummary() {
-		// TODO
-		return null;
+	public ResponseEntity <?> getSummary(
+			Pageable pageable,
+			@RequestParam (name = "firstDate", defaultValue = "") String firstDate,
+			@RequestParam (name = "lastDate", defaultValue = "" ) String lastDate ){
+				Page<SaleSummaryDTO> dto = service.sumaryAmount(pageable, firstDate ,lastDate);
+				return ResponseEntity.ok(dto);
+
 	}
 }
--- src/main/java/com/devsuperior/dsmeta/services/SaleService.java
+++ src/main/java/com/devsuperior/dsmeta/services/SaleService.java
@@ -1,8 +1,17 @@
 package com.devsuperior.dsmeta.services;
 
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.ZoneId;
+import java.time.format.DateTimeParseException;
+import java.util.List;
 import java.util.Optional;
 
+import com.devsuperior.dsmeta.dto.SaleReportDTO;
+import com.devsuperior.dsmeta.dto.SaleSummaryDTO;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import com.devsuperior.dsmeta.dto.SaleMinDTO;
@@ -11,13 +20,44 @@ import com.devsuperior.dsmeta.repositories.SaleRepository;
 
 @Service
 public class SaleService {
-
 	@Autowired
 	private SaleRepository repository;
-	
+
 	public SaleMinDTO findById(Long id) {
 		Optional<Sale> result = repository.findById(id);
 		Sale entity = result.get();
 		return new SaleMinDTO(entity);
+	}
+
+	public Page<SaleReportDTO> findBetweenDate(String firstDate, String lastDate, String sellerMame, Pageable pageable) {
+		LocalDate firstDay;
+		LocalDate lastDay;
+		try {
+			lastDay = LocalDate.parse(lastDate);
+		} catch (DateTimeParseException e) {
+			lastDay = LocalDate.ofInstant(Instant.now(), ZoneId.systemDefault());
+		}
+		try {
+			firstDay = LocalDate.parse(firstDate);
+		} catch (DateTimeParseException e) {
+			firstDay = lastDay.minusYears(1L);
+		}
+		return repository.reportSearch(firstDay, lastDay,sellerMame,  pageable);
+	}
+
+	public Page<SaleSummaryDTO> sumaryAmount(Pageable pageable, String firstDate, String lastDate	) {
+		LocalDate firstDay;
+		LocalDate lastDay;
+		try {
+			lastDay = LocalDate.parse(lastDate);
+		} catch (DateTimeParseException e) {
+			lastDay = LocalDate.ofInstant(Instant.now(), ZoneId.systemDefault());
+		}
+		try {
+			firstDay = LocalDate.parse(firstDate);
+		} catch (DateTimeParseException e) {
+			firstDay = lastDay.minusYears(1L);
+		}
+		return repository.summarySearch(firstDay, lastDay, pageable);
 	}
 }
